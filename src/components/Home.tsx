89d7e0b1eb838422b15248b43ff97072a2d1debd
Wilgot <wilgotwardell@hotmail.com>
2023-01-14 01:03:18 +0100

    login screen

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>BlogIT</h1>
+        <h3>BlogIT is a website that lets you create blogs manly focusing on IT blogs. Here you can post about your ideas, progress and IT projects!</h3>
+        
+    </div>
+  )
+}
---
+++ src/components/Login.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import InputAdornment from '@mui/material/InputAdornment';
+import CircularProgress from '@mui/material/CircularProgress';
+import {BsFillPersonFill} from 'react-icons/bs';
+import {HiLockClosed} from 'react-icons/hi';
+import {MdMail} from 'react-icons/md';
+
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    login();
+  }
+
+  const login = async () => {
+    setLoading(true);
+    const response = await fetch("http://localhost:4000/api/user/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*'
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    })
+
+    //save to localstorage
+    localStorage.setItem('token', await response.json());
+    setLoading(false);
+  }
+
+  return (
+    <div className='form-container'>
+        <form className="login-form" onSubmit={handleSubmit}>
+            <div style={{display: "flex", flexDirection: "column", gap: "50px", width: "100%", justifyContent: "space-around"}}>
+                <div style={{height: "120px", borderBottom: "solid gray 1px", width: "100%"}}>
+                    <h1>Login</h1>
+                </div>
+                <div style={{width: "100%", display: "flex", flexDirection: "column", gap: "20px"}}>
+                        
+                    
+                    <TextField id="outlined-basic" label="email" variant="outlined" value={email} onChange={e => setEmail(e.target.value)} style={{width: "100%"}} InputProps={{
+                        startAdornment: (
+                            <InputAdornment position='start'>
+                                <MdMail style={{height: "20px", width: "20px"}} />
+                            </InputAdornment>
+                        )
+                    }}/>
+                    
+                    <TextField id="outlined-basic" label="password" variant="outlined" value={password} onChange={e => setPassword(e.target.value)} style={{width: "100%"}} InputProps={{
+                        startAdornment: (
+                            <InputAdornment position='start'>
+                                <HiLockClosed style={{height: "20px", width: "20px"}} />
+                            </InputAdornment>
+                        )
+                    }}/>
+                </div>
+                <div style={{width: "100%"}}>
+                    <Button variant={loading ? "outlined" : "contained"} type="submit" value="Submit" style={{width: "80%", height: "40px", borderRadius: "15px", borderColor: "gray", borderWidth: "1px"}}> 
+                        {loading ?
+                            <CircularProgress style={{color: "gray"}} size={24} /> 
+                            :
+                            <span>login</span>
+                        }
+                    </Button>
+                </div>
+                <div>
+                    <span>Not a member? </span>
+                    <a href="">Signup</a>
+                </div>
+            </div>
+        </form>
+    </div>
+  );
+}
+
+export default Login;
+
